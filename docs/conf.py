27f7662b007ff8a5a11dacedc992555a330ee75b
Lenz <lorenz.rumberger@gmail.com>
2024-08-16 11:26:06 +0200

    Revert changes to doc/conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -11,8 +11,7 @@ from importlib.metadata import metadata
 from pathlib import Path
 
 HERE = Path(__file__).parent
-# sys.path.insert(0, str(HERE / "extensions"))
-sys.path.insert(0, str(HERE / "src"))
+sys.path.insert(0, str(HERE / "extensions"))
 
 
 # -- Project information -----------------------------------------------------
